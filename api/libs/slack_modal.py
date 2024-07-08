95e5d17248a6e6068be4c1afbd838d17e4cc05db
jonny graves <jonny.graves@hqo.co>
2024-07-08 12:23:52 -0400

    debug

--- api/libs/slack_modal.py
+++ api/libs/slack_modal.py
@@ -8,67 +8,68 @@ from .logging import init_logger
 LOG = init_logger(os.environ.get("LOG_LEVEL"))
 
 
-def make_modal():
+def make_modal(trigger_id):
     current_year = datetime.datetime.now().year
     current_month = datetime.datetime.now().month
 
     modal_view = {
-        "type": "modal",
-        "callback_id": "modal-identifier",
-        "title": {
-            "type": "plain_text",
-            "text": "Time Off Request"
-        },
-        "submit": {
-            "type": "plain_text",
-            "text": "Submit"
-        },
-        "blocks": [
-            {
-                "type": "section",
-                "block_id": "start-date",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Request some fuckin time off!"
-                },
-                "accessory": {
-                    "type": "datepicker",
-                    "initial_date": f"{current_year}-{current_month}-1",
-                    "placeholder": {
-                        "type": "plain_text",
-                        "text": "Select a start date",
-                        "emoji": True
+        "response_action": "push",
+        "trigger_id": f"{trigger_id}",
+        "view": {
+            "type": "modal",
+            "callback_id": "request-time-off",
+            "title": {
+                "type": "plain_text",
+                "text": "Request time off"
+            },
+            "submit": {
+                "type": "plain_text",
+                "text": "Submit"
+            },
+            "blocks": [
+                {
+                    "type": "section",
+                    "block_id": "start-date",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Request some fuckin time off!"
                     },
-                    "action_id": "datepicker-start"
-                },
-                "accessory": {
-                    "type": "datepicker",
-                    "initial_date": f"{current_year}-{current_month}-1",
-                    "placeholder": {
-                        "type": "plain_text",
-                        "text": "Select an end date",
-                        "emoji": True
+                    "accessory": {
+                        "type": "datepicker",
+                        "initial_date": f"{current_year}-{current_month}-1",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select a start date",
+                            "emoji": True
+                        },
+                        "action_id": "datepicker-start"
                     },
-                    "action_id": "datepicker-end"
+                    "accessory": {
+                        "type": "datepicker",
+                        "initial_date": f"{current_year}-{current_month}-1",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select an end date",
+                            "emoji": True
+                        },
+                        "action_id": "datepicker-end"
+                    }
                 }
-            }
-        ]
+            ]
+        }
     }
 
 # Make the request to open the modal
 def send_modal(trigger_id):
     slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
-    modal_view = make_modal()
+    modal_view = make_modal(trigger_id)
     response = requests.post(
         'https://slack.com/api/views.open',
         headers={
             'Authorization': f'Bearer {slack_bot_token}',
             'Content-Type': 'application/json;charset=utf-8'
         },
-        data=json.dumps({
-            "trigger_id": trigger_id,
-            "view": modal_view
-        })
+        data=json.dumps(modal_view)
     )
     is_json = response.json().get('ok')
     LOG.info(dir(response))
